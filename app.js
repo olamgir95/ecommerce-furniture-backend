73ab998293103fc1de46817c5570e63600026817
Olamgir <ollomurodovolamgir233@gmail.com>
2024-02-03 15:47:53 +0900

    fix: bug fixed related cors

--- app.js
+++ app.js
@@ -26,16 +26,7 @@ app.use(
     origin: true,
   })
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 // 2. Session configuration
 app.use(
   session({
